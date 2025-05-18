8ca90882b0003a7a56d8f7271cf28511a7912b33
Martina Scibilia <martina.scibilia@outlook.com>
2025-05-18 20:08:29 +0200

    Add basic JS

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function generateJoke(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "The poem goes here",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
 
+let poemForm = document.querySelector("#poem-generator");
+poemForm.addEventListener("submit", generateJoke);
